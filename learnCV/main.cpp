27eba464a5324ab9165347e4328c0a303079c6b7
Kingo Wang <kingo@SpringdeMacBook-Pro.local>
2015-08-22 10:57:09 +0800

    first init

--- learnCV/main.cpp
+++ learnCV/main.cpp
@@ -7,9 +7,14 @@
 //
 
 #include <iostream>
-
+#include <opencv2/opencv.hpp>
+#include <string>
+std::string DataPrefix = "/Users/kingo/Documents/Projects/learnCV/learnCV/data/";
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    cv::Mat mat = cv::imread(DataPrefix + "test.jpg");
+    cv::imshow("test", mat);
+    cv::waitKey();
     return 0;
 }
